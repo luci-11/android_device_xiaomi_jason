84568f0b3272df2279902f0a5287454019b10095
Mika-Pikazo <1376106928@qq.com>
2018-07-03 18:49:18 +0800

    jason: Add lineage specified makefiles

---
+++ lineage.mk
@@ -0,0 +1,43 @@
+# 
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from jason device
+$(call inherit-product, device/xiaomi/jason/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_jason
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := jason
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Mi Note 3
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="jason" \
+    PRODUCT_NAME="jason" \
+    PRIVATE_BUILD_DESC="jason-user 8.1.0 OPM1.171019.019 8.6.26 release-keys"
+
+TARGET_VENDOR := Xiaomi
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/jason/jason:8.1.0/OPM1.171019.019/8.6.26:user/release-keys"
